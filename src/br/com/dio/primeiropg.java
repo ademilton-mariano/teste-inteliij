bcc0a9ca3207faa33da42f66fb1233f8bc5f25b4
Ademilton <ademilton.mariano@gmail.com>
2021-09-02 17:51:18 -0300

    first commit

---
+++ src/br/com/dio/primeiropg.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import model.Gato;
+
+public class primeiropg {
+    public static void main(String[] args) {
+        Gato gato= new Gato();
+        System.out.println(gato);
+       /* int a=5;
+        int b =3;
+        System.out.println("Olá Mundo"+ (a+b));*/
+    }
+}
